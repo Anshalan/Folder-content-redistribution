e8e19c47b444c949d2719cf705ad68959fd70ead
Paweł Jędrzejczyk <jedrzejczyk.pawel@outlook.com>
2020-01-29 23:43:48 +0100

    Add move_functions for filetypes, time of execution and reverse file

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-#imports
-import os, sys
+import os, shutil
+import time
 
-####functions#####################################################
-
-####check and create forders you want to use for redistribution############################
 def folderCheckCreate(*therest):
     for x in list(therest):
         if os.path.exists(myDir+x):
-            print("folder", x, "already exist")
+            continue
+            #print("folder", x, "already exist")
         else:
             try:
                 os.mkdir(myDir+x)
@@ -15,22 +13,58 @@ def folderCheckCreate(*therest):
             except os.error as e:
                 print(e)
 
+def moveIfDocument(fileDir, fileName):
+    destFolder = "Documents"
+    if fileName.lower().endswith((".pdf", ".doc",".docx", ".odt", ".txt")):
+        # print(x, 'is a PDF file')
+        folderCheckCreate(destFolder)
+        shutil.move(fileDir + x, fileDir + destFolder +'\\'+ x)
+def moveIfPicture(fileDir, fileName):
+    destFolder = "Pictures"
+    if fileName.lower().endswith((".jpg", ".jpeg",".bmp", ".png")):
+        folderCheckCreate(destFolder)
+        shutil.move(fileDir + x, fileDir + destFolder +'\\'+ x)
+def moveIfExecutable(fileDir, fileName):
+    destFolder = "Applications"
+    if fileName.lower().endswith(".exe"):
+        folderCheckCreate(destFolder)
+        shutil.move(fileDir + x, fileDir + destFolder +'\\'+ x)
+def moveIfISO(fileDir, fileName):
+    destFolder = "Disk images"
+    if fileName.lower().endswith(".iso"):
+        folderCheckCreate(destFolder)
+        shutil.move(fileDir + x, fileDir + destFolder +'\\'+ x)
+def moveIfArchive(fileDir, fileName):
+    destFolder = "Archives"
+    if fileName.lower().endswith((".zip", "rar", ".7z")):
+        folderCheckCreate(destFolder)
+        shutil.move(fileDir + x, fileDir + destFolder + '\\' + x)
+def moveIfSpreadsheet(fileDir, fileName):
+    destFolder = "Spreadsheets"
+    if fileName.lower().endswith((".xml", "xls", "xlsm", "xlsx")):
+        folderCheckCreate(destFolder)
+        shutil.move(fileDir + x, fileDir + destFolder + '\\' + x)
+def moveIfCode(fileDir, fileName):
+    destFolder = "Code"
+    if fileName.lower().endswith((".py", ".c", ".cpp", ".h", ".hpp")):
+        folderCheckCreate(destFolder)
+        shutil.move(fileDir + x, fileDir + destFolder + '\\' + x)
 
-myDir = "C:\\Users\\jedrz\\Downloads\\Folder testowy\\" ##drectory you want to redistribute files from as "C:\\aaa\\sss\\ddd\\"
+start_time = time.process_time()
+
+myDir = "C:\\Users\\jedrz\\Downloads\\" ##directory you want to redistribute files from as "C:\\aaa\\sss\\ddd\\"
 dirlist = os.listdir(myDir)
-#print (dirlist)
 
-#### listing directory content########################################
-# for x in os.listdir("."):
-# for x in dirlist :
-#     #print (myDir + x)
-#     if os.path.isfile(myDir+x):
-#         print ('f- ', x)
-#     elif os.path.isdir(myDir+x):
-#         print ("d- ", x)
-#     else:
-#         print("nothing?")
+for x in dirlist :
+    if os.path.isfile(myDir+x):
+        moveIfDocument(myDir, x)
+        moveIfArchive(myDir, x)
+        moveIfCode(myDir, x)
+        moveIfExecutable(myDir, x)
+        moveIfISO(myDir, x)
+        moveIfPicture(myDir, x)
+        moveIfSpreadsheet(myDir, x)
 
-
-
-folderCheckCreate("test2", "test3")
+    else:
+        print(x, "is not a file but and didnt tell me what to do with it")
+print("---%s seconds ---" % (time.process_time() -start_time))
---
+++ reverse.py
@@ -0,0 +1,15 @@
+import os, shutil
+import time
+start_time = time.process_time()
+def reverse(dirPath, folderNames):
+    for x in list(folderNames):
+        if os.path.exists(dirPath+"\\"+x):
+            for file in os.listdir(dirPath+"\\"+x):
+                shutil.move(dirPath+"\\"+x+"\\"+file, dirPath+"\\"+file)
+            os.rmdir(dirPath+"\\"+x)
+
+myDir = "C:\\Users\\jedrz\\Downloads\\"
+folderNames = ("Documents", "Pictures", "Applications", "Disk images", "Archives","Spreadsheets","Code")
+
+reverse(myDir,folderNames)
+print("---%s seconds ---" % (time.process_time() -start_time))
