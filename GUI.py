21a1429d12e3d67e19d904f3b6899a97822fb89d
unknown <jedrzejczyk.pawel@outloog.com>
2020-02-01 16:05:00 +0100

    add reverse to GUI, visual changes in GUI

--- GUI.py
+++ GUI.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, Text
 import move
+from reverse import reverseMove
 
 folderName = ""
 
@@ -12,20 +13,28 @@ def addFolder():
     # print(folder)
 def execute():
     move.tempFunctionName(folderName)
+    print("moved")
+def rev():
+    reverseMove(folderName,("Documents", "Images", "Applications", "Disk images", "Archives","Spreadsheets","Code"))
+    print("reversed")
 
 # def getDirectory():
 window = tk.Tk()
 window.title("Files redistributon")
-canvas = tk.Canvas(window, height=500, width=800, bg="#320775")
+
+
+canvas = tk.Canvas(window, height=250, width=250, bg="#c1c1c1")
 canvas.pack()
+frame = tk.Frame(window, bg= "#B1B1B1", bd=1, highlightbackground="black")
 
-frame = tk.Frame(window, bg= "#D8A5F3")
-frame.place(relheight=0.7, relwidth=0.7, relx=0.15, rely=0.15)
+frame.place(relheight=0.8, relwidth=0.8, relx=0.1, rely=0.1)
 
-openFile =tk.Button(frame, text="Select folder", bg="#320775", fg = "white", activebackground="#32FF75", command = addFolder)
+openFile =tk.Button(frame, text="Select folder", bg="#C6C6C6", fg = "black", activebackground="#32FF75", command = addFolder)
 openFile.pack()
-runApp =tk.Button(frame, text="Run App", bg="#320775", fg = "white", activebackground="#32FF75", command = execute)
+runApp =tk.Button(frame, text="Run App", bg="#C6C6C6", fg = "black", activebackground="#32FF75", command = execute)
 runApp.pack()
+Reverse =tk.Button(frame, text="Reverse", bg="#C6C6C6", fg = "black", activebackground="#32FF75", command = rev)
+Reverse.pack()
 
 window.mainloop()
     # return folderName
--- reverse.py
+++ reverse.py
@@ -1,18 +1,19 @@
 import os, shutil
 import time
-import GUI
 
-start_time = time.process_time()
-def reverse(dirPath, folderNames):
+# start_time = time.process_time()
+def reverseMove(dirPath, folderNames):
+    start_time = time.process_time()
     for x in list(folderNames):
         if os.path.exists(dirPath+"\\"+x):
             for file in os.listdir(dirPath+"\\"+x):
                 shutil.move(dirPath+"\\"+x+"\\"+file, dirPath+"\\"+file)
             os.rmdir(dirPath+"\\"+x)
+    print("---%s seconds ---" % (time.process_time() -start_time))
 
-# myDir = GUI.getDirectory()+"/"
-myDir ="C:\\Users\\jedrz\\Downloads\\"
-folderNames = ("Documents", "Images", "Applications", "Disk images", "Archives","Spreadsheets","Code")
-
-reverse(myDir,folderNames)
-print("---%s seconds ---" % (time.process_time() -start_time))
+# # myDir = GUI.getDirectory()+"/"
+# myDir ="C:\\Users\\jedrz\\Downloads\\"
+# folderNames = ("Documents", "Images", "Applications", "Disk images", "Archives","Spreadsheets","Code")
+#
+# reverseMove(myDir,folderNames)
+# print("---%s seconds ---" % (time.process_time() -start_time))
