5624fdaf21a6c0a08a795a7beba61ff722be734d
Paweł Jędrzejczyk <jedrzejczyk.pawel@outlook.com>
2020-02-02 01:17:12 +0100

    add states on buttons

--- GUI.py
+++ GUI.py
@@ -10,6 +10,9 @@ def addFolder():
     folderName = filedialog.askdirectory()
     # folder.append(folderName)
     print(folderName)
+    dirShow.configure(text = folderName)
+    runApp.configure(state='normal')
+    Reverse.configure(state='normal')
     # print(folder)
 def execute():
     move.tempFunctionName(folderName)
@@ -31,10 +34,13 @@ frame.place(relheight=0.8, relwidth=0.8, relx=0.1, rely=0.1)
 
 openFile =tk.Button(frame, text="Select folder", bg="#C6C6C6", fg = "black", activebackground="#32FF75", command = addFolder)
 openFile.pack()
-runApp =tk.Button(frame, text="Run App", bg="#C6C6C6", fg = "black", activebackground="#32FF75", command = execute)
+dirShow =tk.Label(frame, text="Choose a foder",bg="#B1B1B1")
+dirShow.pack()
+runApp =tk.Button(frame, text="Run App", state='disabled', bg="#C6C6C6", fg = "black", activebackground="#32FF75", command = execute)
 runApp.pack()
-Reverse =tk.Button(frame, text="Reverse", bg="#C6C6C6", fg = "black", activebackground="#32FF75", command = rev)
+Reverse =tk.Button(frame, text="Reverse", state='disabled', bg="#C6C6C6", fg = "black", activebackground="#32FF75", command = rev)
 Reverse.pack()
+
 
 window.mainloop()
     # return folderName
